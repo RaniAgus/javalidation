c6c0115d26a4e30d2e4e66c8f406dc4e69391a42
Agustin Ranieri <aguseranieri@gmail.com>
2026-02-07 03:37:07 -0300

    Rename TemplateString field from 'template' to 'message' and update related references

--- src/main/java/io/github/raniagus/javalidation/template/TemplateString.java
+++ src/main/java/io/github/raniagus/javalidation/template/TemplateString.java
@@ -4,29 +4,29 @@ import java.util.Arrays;
 import java.util.Objects;
 import org.jspecify.annotations.NonNull;
 
-public record TemplateString(String template, Object... values) {
+public record TemplateString(String message, Object... args) {
     public TemplateString {
-        values = Arrays.copyOf(values, values.length);
+        args = Arrays.copyOf(args, args.length);
     }
 
     @Override
     public @NonNull String toString() {
         return "TemplateString{" +
-                "template='" + template + '\'' +
-                ", values=" + Arrays.toString(values) +
+                "message='" + message + '\'' +
+                ", args=" + Arrays.toString(args) +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof TemplateString(String template1, Object[] values1))) return false;
-        return Objects.equals(template(), template1) && Arrays.equals(values(), values1);
+        return Objects.equals(message(), template1) && Arrays.equals(args(), values1);
     }
 
     @Override
     public int hashCode() {
-        int result = Objects.hashCode(template());
-        result = 31 * result + Arrays.hashCode(values());
+        int result = Objects.hashCode(message());
+        result = 31 * result + Arrays.hashCode(args());
         return result;
     }
 }
