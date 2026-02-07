3061dd577a48a1c4672572174dd5b1d7a7432d1b
Agustin Ranieri <aguseranieri@gmail.com>
2026-02-07 03:43:22 -0300

    Update ValidationException to support message formatting with arguments

--- src/main/java/io/github/raniagus/javalidation/ValidationException.java
+++ src/main/java/io/github/raniagus/javalidation/ValidationException.java
@@ -3,28 +3,16 @@ package io.github.raniagus.javalidation;
 public class ValidationException extends RuntimeException {
     private final ValidationErrors errors;
 
-    public ValidationException(String message) {
-        super(message);
-        this.errors = ValidationErrors.of(message);
-    }
-
-    public ValidationException(String message, Throwable cause) {
-        super(message, cause);
-        this.errors = ValidationErrors.of(message);
-    }
-
-    public ValidationException(String field, String message) {
-        super(field + ": " + message);
-        this.errors = ValidationErrors.of(field, message);
-    }
-
-    public ValidationException(String field, String message, Throwable cause) {
-        super(field + ": " + message, cause);
-        this.errors = ValidationErrors.of(field, message);
-    }
-
     public ValidationException(ValidationErrors errors) {
         this.errors = errors;
+    }
+
+    public ValidationException(String message, Object... args) {
+        this(ValidationErrors.of(message, args));
+    }
+
+    public ValidationException(String field, String message, Object... args) {
+        this(ValidationErrors.of(field, message, args));
     }
 
     public ValidationErrors getErrors() {
