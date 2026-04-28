d31ab11c69b2ab1414244844d305e99a0cd58594
Agustin Ranieri <aguseranieri@gmail.com>
2026-04-28 19:20:44 -0300

    refactor: optimize message formatting by caching MessageFormat instances

--- javalidation/src/main/java/io/github/raniagus/javalidation/format/MessageFormatTemplateStringFormatter.java
+++ javalidation/src/main/java/io/github/raniagus/javalidation/format/MessageFormatTemplateStringFormatter.java
@@ -2,10 +2,17 @@ package io.github.raniagus.javalidation.format;
 
 import io.github.raniagus.javalidation.TemplateString;
 import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MessageFormatTemplateStringFormatter implements TemplateStringFormatter {
+    private static final ThreadLocal<Map<String, MessageFormat>> CACHE =
+        ThreadLocal.withInitial(HashMap::new);
+
     @Override
     public String format(TemplateString template) {
-        return MessageFormat.format(template.message(), template.args());
+        MessageFormat mf = CACHE.get()
+            .computeIfAbsent(template.message(), MessageFormat::new);
+        return mf.format(template.args());
     }
 }
