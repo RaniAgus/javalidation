5f934ea12bcd820e56a25b0473b64317f0c44d74
Agustin Ranieri <aguseranieri@gmail.com>
2026-04-30 00:25:12 -0300

    refactor: simplify MessageFormatTemplateStringFormatter by removing caching logic

--- javalidation/src/main/java/io/github/raniagus/javalidation/format/MessageFormatTemplateStringFormatter.java
+++ javalidation/src/main/java/io/github/raniagus/javalidation/format/MessageFormatTemplateStringFormatter.java
@@ -2,17 +2,10 @@ package io.github.raniagus.javalidation.format;
 
 import io.github.raniagus.javalidation.TemplateString;
 import java.text.MessageFormat;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MessageFormatTemplateStringFormatter implements TemplateStringFormatter {
-    private static final ThreadLocal<Map<String, MessageFormat>> CACHE =
-        ThreadLocal.withInitial(HashMap::new);
-
     @Override
     public String format(TemplateString template) {
-        MessageFormat mf = CACHE.get()
-            .computeIfAbsent(template.message(), MessageFormat::new);
-        return mf.format(template.args());
+        return MessageFormat.format(template.message(), template.args());
     }
 }
